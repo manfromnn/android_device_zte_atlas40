884d0a10d7eba066c427aeed22689f170b5ac4cd
Andrej <manfromnn@gmail.com>
2014-07-28 15:24:44 +0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,22 +1,20 @@
 # Boot animation
-TARGET_SCREEN_HEIGHT := 800
-TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/zte/atlas40/atlas40.mk)
+$(call inherit-product, device/zte/roamer2/roamer2.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := atlas40
-PRODUCT_NAME := cm_atlas40
+PRODUCT_DEVICE := roamer2
+PRODUCT_NAME := cm_roamer2
 PRODUCT_BRAND := ZTE
-PRODUCT_MODEL := Blade III
+PRODUCT_MODEL := V790(KIS III)
 PRODUCT_MANUFACTURER := ZTE
-PRODUCT_RELEASE_NAME := Blade III
+PRODUCT_RELEASE_NAME := V790(KIS III)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="ZTE/N880E_JB4_2/atlas40:4.2/JOP40C/20121121.110335:user/release-keys" \
-    PRIVATE_BUILD_DESC="N880E_JB4_2-user 4.2 JOP40C 20121121.110335 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=roamer2 BUILD_ID=JRO03C BUILD_FINGERPRINT=ZTE/V790_JB4_2/roamer2:4.2/JOP40C/20121121.110335:user/release-keys PRIVATE_BUILD_DESC="V790_JB4_2-user 4.2 JOP40C 20121121.110335 release-keys" BUILD_NUMBER=20121121.110335
