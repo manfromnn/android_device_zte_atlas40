1ff056de3239f32cf5d965fb4b4727a378a92605
Andrej <manfromnn@gmail.com>
2014-12-17 22:14:27 +0300

    Update and rename atlas40.mk to roamer2.mk

--- roamer2.mk
+++ roamer2.mk
@@ -2,13 +2,13 @@ $(call inherit-product, device/qcom/common/common.mk)
 
 # Ramdisk
 PRODUCT_COPY_FILES := \
-    $(call find-copy-subdir-files,*,device/zte/atlas40/ramdisk,root)
+    $(call find-copy-subdir-files,*,device/zte/roamer2/ramdisk,root)
 
 # Prebuilt
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,device/zte/atlas40/prebuilt/system,system)
+    $(call find-copy-subdir-files,*,device/zte/roamer2/prebuilt/system,system)
 
-$(call inherit-product-if-exists, vendor/zte/atlas40/atlas40-vendor-blobs.mk)
+$(call inherit-product-if-exists, vendor/zte/roamer2/roamer2-vendor-blobs.mk)
 $(call inherit-product-if-exists, vendor/qcom/common/vendor-blobs.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
@@ -23,12 +23,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.moz.omx.hw.max_width=640 \
     ro.moz.omx.hw.max_height=360 \
     ro.moz.fm.noAnalog=true \
-    ro.sf.lcd_density=240
+    ro.sf.lcd_density=160
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := atlas40
-PRODUCT_DEVICE := atlas40
+PRODUCT_NAME := roamer2
+PRODUCT_DEVICE := roamer2
 PRODUCT_BRAND := ZTE
 PRODUCT_MANUFACTURER := ZTE
-PRODUCT_MODEL := atlas40
+PRODUCT_MODEL := roamer2
 ENABLE_LIBRECOVERY := true
